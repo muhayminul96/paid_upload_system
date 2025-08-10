f29cd8dcd3b5c2794859a6f2e3a2ebce6ede7f49
muhayminul96 <muhayminul96@gmail.com>
2025-08-10 22:11:35 +0600

    added successfully

--- filepay/admin.py
+++ filepay/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
-from .models import FileUpload, ActivityLog, PaymentTransaction
+from .models import FileUpload, PaymentTransaction, ActivityLog
 
-# Register your models here.
+class ReadOnlyAdminMixin:
 
-site = admin.AdminSite(name='file pay')
+    def has_add_permission(self, request):
+        return False
 
-admin.site.register(ActivityLog)
-admin.site.register(FileUpload)
-admin.site.register(PaymentTransaction)
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+@admin.register(FileUpload)
+class FileUploadAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ('id', 'user', 'filename', 'word_count', 'status', 'upload_time')
+    readonly_fields = ('user', 'filename','file', 'word_count', 'status', 'upload_time')
+
+@admin.register(PaymentTransaction)
+class PaymentTransactionAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ('id', 'user', 'transaction_id', 'amount', 'status', 'timestamp')
+    readonly_fields = ('user', 'transaction_id', 'amount','gateway_response', 'status', 'timestamp')
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
+    list_display = ('id', 'user', 'action', 'metadata', 'timestamp')
+    readonly_fields = ('user', 'action', 'metadata', 'timestamp')
